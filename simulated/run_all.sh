3edef1b6bab85a467e90f3a6c418231c54be1b2c
Tamas Spisak <tamas.spisak@uk-essen.de>
2021-10-18 15:09:58 +0200

    new simulation cases added

--- simulated/run_all.sh
+++ simulated/run_all.sh
@@ -15,6 +15,12 @@ python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e0_linear.csv --
 
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d.5e0_linear.csv --mode=partial --delta=0.5 --epsilon=0 --nonlin-trf=identity --repetitions=100
 
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d.5e.5_linear.csv --mode=partial --delta=0.5 --epsilon=0.5 --nonlin-trf=identity --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d01e0_linear.csv --mode=partial --delta=0.1 --epsilon=0 --nonlin-trf=identity --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d01e.5_linear.csv --mode=partial --delta=0.1 --epsilon=0.5 --nonlin-trf=identity --repetitions=100
+
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e0_sigmoid.csv --mode=partial --delta=1 --epsilon=0 --nonlin-trf=sigmoid --repetitions=100
 
 
