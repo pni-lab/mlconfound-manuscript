fdb6ff5333af545b5b10e0de5b8c1223ac19d0d6
Tamas Spisak <tamas.spisak@uk-essen.de>
2021-10-22 10:17:00 +0200

    new simulation cases: sigmoid

--- simulated/run_all.sh
+++ simulated/run_all.sh
@@ -29,8 +29,6 @@ python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d01e0_linear.csv -
 
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d01e.5_linear.csv --mode=partial --delta=0.1 --epsilon=0.5 --nonlin-trf=identity --repetitions=100
 
-python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e0_sigmoid.csv --mode=partial --delta=1 --epsilon=0 --nonlin-trf=sigmoid --repetitions=100
-
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e1_linear.csv --mode=partial --delta=1 --epsilon=1 --nonlin-trf=identity --repetitions=100
 
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_linear.csv --mode=partial --delta=1.05 --epsilon=3 --nonlin-trf=identity --repetitions=100
@@ -40,5 +38,15 @@ python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_linear.csv
 python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_linear.csv --mode=partial --delta=5 --epsilon=10 --nonlin-trf=identity --repetitions=100
 
 
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e0_sigmoid.csv --mode=partial --delta=1 --epsilon=0 --nonlin-trf=sigmoid --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1e1_sigmoid.csv --mode=partial --delta=1 --epsilon=1 --nonlin-trf=sigmoid --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_sigmoid.csv --mode=partial --delta=1.05 --epsilon=3 --nonlin-trf=sigmoid --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_sigmoid.csv --mode=partial --delta=1.5 --epsilon=5 --nonlin-trf=sigmoid --repetitions=100
+
+python3 $DIR/simulate_H1.py --out=$DIR/results/h1_ccc_partial_d1.05e3_sigmoid.csv --mode=partial --delta=5 --epsilon=10 --nonlin-trf=sigmoid --repetitions=100
 
 
